cd3751a1bf909c6aba65e1ebfb25d9a9911d6c8f
Omar Samy <omar.samy3333@gmail.com>
2022-09-10 22:27:16 +0200

    First commit to add singlton pattern(one thread or multithreading

---
+++ Design Patterns/Program.cs
@@ -0,0 +1,62 @@
+﻿using Design_Patterns.Singleton_Pattern;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Singleton Pattern
+            //OneThread();
+            MultiThreading();
+            #endregion
+        }
+
+        #region Singleton Pattern
+
+        public static void OneThread()
+        {
+            #region One Thread
+            Counter counter = Counter.GetInstance();
+            Console.WriteLine(counter.count);
+            counter.AddOne();
+            Console.WriteLine(counter.count);
+
+            Console.WriteLine("*********************************");
+
+            Counter counter0 = Counter.GetInstance();
+            Console.WriteLine(counter0.count);
+            counter0.AddOne();
+            Console.WriteLine(counter0.count);
+            #endregion
+        }
+
+        public static void MultiThreading()
+        {
+            #region Multihreading
+
+            Task Task1 = Task.Factory.StartNew(() =>
+            {
+                Counter counter1 = Counter.GetInstance();
+                counter1.AddOne();
+                Console.WriteLine("counter 1: " + counter1.count);
+            });
+
+
+            Task Task2 = Task.Factory.StartNew(() =>
+            {
+                Counter counter2 = Counter.GetInstance();
+                counter2.AddOne();
+                Console.WriteLine("counter 2: " + counter2.count);
+            });
+
+            Console.ReadKey();
+            #endregion
+        }
+
+        #endregion
+
+    }
+}
---
+++ Design Patterns/Singleton Pattern/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Singleton_Pattern
+{
+    public class Counter
+    {
+        public int count;
+        private static Counter counter = null;
+        private static object LockObj = new object();
+            
+        private Counter(){ }
+        public static Counter GetInstance()
+        {
+            //Using double check to control multithreading.
+            if (counter == null)
+            {
+                lock (LockObj) //lock to excute one thread first.
+                {
+                    if (counter == null) counter = new Counter();
+                }
+            }
+            return counter;
+        }
+        public void AddOne() { count++; }
+    }
+}
